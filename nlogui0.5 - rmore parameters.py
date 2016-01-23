8277ab0467504401704e89eee20443b27e288ba6
danhickstein <danhickstein@gmail.com>
2016-01-22 17:15:21 -0700

    added betas

--- nlogui0.5 - rmore parameters.py
+++ nlogui0.5 - rmore parameters.py
@@ -34,7 +34,6 @@ def dB(num):
     return 10*np.log10(np.abs(num)**2)
     
     
-    
 fig = plt.figure(figsize=(15,9))
 
 axL1 = plt.subplot2grid((3,4), (2,0))
@@ -56,9 +55,14 @@ plt.subplots_adjust(left=0.05,bottom=0.09,right=0.96,top=0.96,wspace=0.3,hspace=
 axRun = plt.axes([0.4,0.90,0.09,0.04])
 bRun = Button(axRun, 'Run Simulation')
 
+axCalc = plt.axes([0.4,0.35,0.09,0.04])
+bCalc = Button(axCalc, 'Calc Dispersion')
+
+
+
 
 ###Parameter axes###
-sl = 0.2
+sl = 0.13
 sw = 0.1
 sh = 0.04
 sv = 0.97 #start vertical
@@ -69,20 +73,31 @@ ax02 = plt.axes([sl, sv+2*ss, sw, sh])
 ax03 = plt.axes([sl, sv+3*ss, sw, sh])
 
 ax04 = plt.axes([sl, sv+5*ss, sw, sh])
+ax05 = plt.axes([sl, sv+6*ss, sw, sh])
+ax06 = plt.axes([sl, sv+7*ss, sw, sh])
 
-ax05 = plt.axes([sl, sv+7*ss, sw, sh])
-ax06 = plt.axes([sl, sv+8*ss, sw, sh])
-ax07 = plt.axes([sl, sv+9*ss, sw, sh])
+ax07 = plt.axes([sl, sv+9* ss, sw, sh])
+ax08 = plt.axes([sl, sv+10*ss, sw, sh])
+ax09 = plt.axes([sl, sv+11*ss, sw, sh])
+ax10 = plt.axes([sl, sv+12*ss, sw, sh])
+ax11 = plt.axes([sl, sv+13*ss, sw, sh])
+
 
 bPulse = TextBox(ax01,'Pulse duration (ps)',   initial='0.0284')
 bWave  = TextBox(ax02,'Center wavelength (nm)',initial='835')
 bPower = TextBox(ax03,'Pulse peak power (kW)', initial='10') # should change to pulse energy
 
-bLength = TextBox(ax04,'Fiber length (mm)',    initial='15') 
+bWindow= TextBox(ax04,'Time window (ps)',  initial='10.0') 
+bSteps = TextBox(ax05,'Steps',             initial='50') 
+bPoints= TextBox(ax06,'Simulation points', initial='2**13')
 
-bDz    = TextBox(ax05,'dz',                    initial='0.0001') 
-bSteps = TextBox(ax06,'Steps',                 initial='50') 
-bPoints = TextBox(ax07,'Simulation points',    initial='2**13')
+bLength= TextBox(ax07,'Fiber length (mm)', initial='15')
+bGamma = TextBox(ax08,'Gamma (1/(W km))',  initial='11') 
+bBeta2 = TextBox(ax09,'Beta_2 (ps^2/km)',  initial='-7.7') 
+bBeta3 = TextBox(ax10,'Beta_3 (ps^3/km)',  initial='0.05') 
+bFibWL = TextBox(ax11,'Center WL (nm)',    initial='1000')
+
+buttons = (bPulse, bWave, bPower, bWindow, bSteps, bPoints, bLength, bGamma, bBeta2, bBeta3, bFibWL)
 
 
 axL1.set_xlabel('Frequency (THz)')
@@ -118,19 +133,26 @@ def run_simulation(caller=None,prop=False):
     centerwl          = float(bWave.text) # in nm
     pump_power        = float(bPower.text) * 1e3
     
-    fiber_length = float(bLength.text) * 1e-3
-
-    dz    = float(bDz.text)
-    steps = int(bSteps.text)
-
+    window  = float(bWindow.text)
+    steps   = int(bSteps.text)
     npoints = int(eval(bPoints.text))
     
+    fiber_length = float(bLength.text) * 1e-3
+    gamma = float(bGamma.text)
+    beta2 = float(bBeta2.text)
+    beta3 = float(bBeta3.text)
+    fibWL = float(bFibWL.text)
+    
+    
+    
     # set up the pulse parameters
-    pulse = SechPulse(pump_power, pump_pulse_length, centerwl, time_window=10.0,
+    pulse = SechPulse(pump_power, pump_pulse_length, centerwl, time_window=window,
                         GDD=0, TOD=0.0, NPTS=npoints, frep_MHz=100, power_is_avg=False)
 
     fiber1 = fiber.FiberInstance() 
-    fiber1.load_from_db(fiber_length, 'dudley')
+    fiber1.generate_fiber(fiber_length,center_wl_nm=fibWL,betas=(beta2*1e-3,beta3*1e-3),gamma_W_m=gamma*1e-3,gvd_units='ps^n/m')
+    
+    # fiber1.load_from_db(fiber_length, 'dudley')
     
     print 'Parameters\n------------------'
     print '--Pulse--'
@@ -138,15 +160,20 @@ def run_simulation(caller=None,prop=False):
     print 'Center wl (nm) %g'%centerwl
     print 'Pump power (W) %g'%pump_power
     print '  Energy (nJ)  %f'%(pulse.calc_epp()*1e9)
-    print '--Fiber--'
-    print 'Length (mm)     %f'%(fiber_length*1e3)
     print '--Simulation--'
-    print 'dz             %f'%dz
+    print 'Window (ps)    %f'%window
     print 'Steps          %i'%steps
     print 'Time points    %i'%npoints
+    print '--Fiber--'
+    print 'Length (mm)     %f'%(fiber_length*1e3)
+    print 'Gamma (W-1km-1) %f'%(gamma)
+    print 'Beta2 (ps^2/km) %f'%(beta2)
+    print 'Beta3 (ps^3/km) %f'%(beta3)
+    print 'FiberWL (nm)    %f'%(fibWL)
+    
+    
     
 
-    
     W = pulse.W_mks
     T = pulse.T_ps
     xW = W[W>0]
@@ -173,7 +200,7 @@ def run_simulation(caller=None,prop=False):
     bRun.label.set_text('Running...'); drawnow()
     
     # set up the propagation parameters
-    evol = SSFM.SSFM(dz=1e100, local_error=0.001, USE_SIMPLE_RAMAN=True)
+    evol = SSFM.SSFM(local_error=0.001, USE_SIMPLE_RAMAN=True)
     
     # propagate the pulse!
     y, AW, AT, pulse1 = evol.propagate(pulse_in=pulse, fiber=fiber1, n_steps=steps)
@@ -194,7 +221,7 @@ def run_simulation(caller=None,prop=False):
     
     extent = (np.min(xW),np.max(xW),np.min(y),np.max(y[:-1]))
     ax3.imshow(zW, extent=extent, vmin = np.max(zW) - 40.0, vmax = np.max(zW),aspect='auto',origin='lower')
-    ax3.set_xlabel('Wavelength (nm)')
+    ax3.set_xlabel('Frequency (THz)')
     ax3.set_ylabel('Distance (m)')
     
     
@@ -225,9 +252,10 @@ def run_simulation_full(caller=None):
     run_simulation(prop=True)
 
 
-for box in (bPulse, bWave, bPower, bLength, bDz, bSteps, bPoints):
-    box.on_submit(run_simulation)
+# for box in buttons:
+#     box.on_submit(run_simulation)
 
+bCalc.on_clicked(run_simulation)
 bRun.on_clicked(run_simulation_full)
 
 run_simulation(prop=False)
@@ -236,7 +264,6 @@ reset_plots()
 
 
 drawnow()
-
 
 
 
